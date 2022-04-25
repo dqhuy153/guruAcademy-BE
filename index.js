811ca9b3d31a93b8fbef13b83efc5450f8174471
Duong Lam <duonglam.dev@gmail.com>
2022-04-25 21:35:50 +0700

    Intergated socket server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const http = require('http')
 require('dotenv').config()
 
 const authRoutes = require('./routers/auth')
@@ -24,6 +25,10 @@ const adminUsersRoutes = require('./routers/admin/users')
 const app = express()
 const PORT = process.env.PORT || 5000
 const mongodbUri = process.env.MONGODB_URI
+
+// create socket server
+const server = http.createServer(app)
+const io = require('socket.io')(server)
 
 //middleware
 app.use(cors())
@@ -64,6 +69,60 @@ app.use((error, req, res, next) => {
     data: errorData,
     success: false,
     statusCode: status,
+  })
+})
+
+const connections = {}
+io.on('connection', (socket) => {
+  console.log(connections)
+  socket.on('join-call', (clientPath) => {
+    if (connections[clientPath] === undefined) {
+      connections[clientPath] = {}
+    }
+
+    connections[clientPath][socket.id] = {}
+    connections[clientPath][socket.id].accessTime = new Date()
+    console.log(connections[clientPath])
+    Object.keys(connections[clientPath]).forEach((userId) => {
+      io.to(userId).emit('user-joined', socket.id, connections[clientPath])
+    })
+  })
+
+  socket.on('signal', (toId, message) => {
+    io.to(toId).emit('signal', socket.id, message)
+  })
+
+  socket.on('chat-message', (data, sender, clientPath) => {
+    data = sanitizeString(data)
+    sender = sanitizeString(sender)
+
+    Object.keys(connections[clientPath]).forEach((clientId) => {
+      io.to(clientId).emit(
+        'chat-message',
+        data,
+        sender,
+        socket.id
+      )
+    })
+  })
+
+  socket.on('disconnect', () => {
+    let room = ''
+    Object.keys(connections).forEach((clientRoom) => {
+      if (!connections[clientRoom][socket.id]) return
+      room = clientRoom
+      delete connections[clientRoom][socket.id]
+    })
+
+    if (Object.keys(connections[room])?.length === 0) {
+      delete connections[room]
+
+      return
+    }
+
+    Object.keys(connections[room])?.forEach((userId) => {
+      io.to(userId).emit('user-left', socket.id)
+    })
   })
 })
 
